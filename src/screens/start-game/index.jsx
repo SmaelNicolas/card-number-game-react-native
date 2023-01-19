61e3f41cfe703544dd94b8751e3d0a54778cc9ef
Smael Nicolas <nico.smael@gmail.com>
2023-01-18 22:34:39 -0300

    added handle to input , keyborad dismiss

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,33 +1,53 @@
-import React from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+	Button,
+	Keyboard,
+	Text,
+	TextInput,
+	TouchableWithoutFeedback,
+	View,
+} from "react-native";
 import { Card } from "../../components";
 import { colors } from "../../constants/themes/colors";
 import { styles } from "./styles";
 
 export const StartGame = () => {
+	const [enteredValue, setEnteredValue] = useState("");
+
+	const onHandlerChange = (text) => {
+		setEnteredValue(text.replace(/[^0-9]/g, ""));
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Comenzar Juego</Text>
-			<Card style={styles.inputContainer}>
-				<Text style={styles.label}>Escribe un numero</Text>
-				<TextInput
-					placeholder="0"
-					style={styles.input}
-					keyboardType="numeric"
-				/>
-				<View style={styles.buttonContainer}>
-					<Button
-						color={colors.primary}
-						title="Reiniciar"
-						onPress={() => console.log()}
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}>
+			<View style={styles.container}>
+				<Text style={styles.title}>Comenzar Juego</Text>
+				<Card style={styles.inputContainer}>
+					<Text style={styles.label}>Escribe un numero</Text>
+					<TextInput
+						value={enteredValue}
+						onChangeText={onHandlerChange}
+						placeholder="0"
+						style={styles.input}
+						keyboardType="numeric"
 					/>
-					<Button
-						color={colors.primary}
-						title="Confirmar"
-						onPress={() => console.log()}
-					/>
-				</View>
-			</Card>
-		</View>
+					<View style={styles.buttonContainer}>
+						<Button
+							color={colors.primary}
+							title="Reiniciar"
+							onPress={() => console.log()}
+						/>
+						<Button
+							color={colors.primary}
+							title="Confirmar"
+							onPress={() => console.log()}
+						/>
+					</View>
+				</Card>
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
