5e70bff47fe574d9c82311d4428dbf679190a231
Smael Nicolas <nico.smael@gmail.com>
2023-01-18 21:23:37 -0300

    create layout start screen

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
 export const StartGame = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Start Game</Text>
+			<Text style={styles.title}>Comenzar Juego</Text>
+			<View style={styles.inputContainer}>
+				<Text style={styles.label}>Escribe un numero</Text>
+				<TextInput placeholder="0" style={styles.input} />
+			</View>
+			<View style={styles.buttonContainer}>
+				<Button
+					color="#c4e4e9"
+					title="Reiniciar"
+					onPress={() => console.log()}
+				/>
+				<Button
+					color="#97a1e8"
+					title="Confirmar"
+					onPress={() => console.log()}
+				/>
+			</View>
 		</View>
 	);
 };
--- src/screens/start-game/styles.js
+++ src/screens/start-game/styles.js
@@ -5,4 +5,35 @@ export const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: "center",
 	},
+	title: {
+		fontSize: 18,
+		color: "#212121",
+		textAlign: "center",
+		paddingVertical: 31,
+	},
+	inputContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		marginHorizontal: 15,
+	},
+	label: {
+		fontSize: 18,
+		color: "#212121",
+		textAlign: "center",
+		paddingVertical: 31,
+	},
+	input: {
+		width: "100%",
+		borderBottomColor: "#97a1d8",
+		minWidth: 70,
+		fontSize: 22,
+		paddingVertical: 10,
+		textAlign: "center",
+	},
+	buttonContainer: {
+		flexDirection: "row",
+		width: "75%",
+		justifyContent: "space-around",
+		marginTop: 20,
+	},
 });
