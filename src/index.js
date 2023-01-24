9cf63bb4748fc26b54548a9c201fd41600ee891a
Smael Nicolas <nico.smael@gmail.com>
2023-01-24 15:58:09 -0300

    added custom hook

--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import { useState } from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 import { Header } from "./components";
 import { colors } from "./constants/themes/colors";
+import { useFontCustom } from "./hooks/useFont";
 import { GameScreen, StartGame, WelcomeScreen } from "./screens";
 import { styles } from "./styles";
 
 export const App = () => {
-	const [loaded, error] = useFonts({
-		"Raleway-Thin": require("../assets/fonts/Raleway-Thin.ttf"),
-		"Raleway-ExtraLight": require("../assets/fonts/Raleway-ExtraLight.ttf"),
-		"Raleway-Light": require("../assets/fonts/Raleway-Light.ttf"),
-		"Raleway-Medium": require("../assets/fonts/Raleway-Medium.ttf"),
-		"Raleway-Regular": require("../assets/fonts/Raleway-Regular.ttf"),
-		"Raleway-SemiBold": require("../assets/fonts/Raleway-SemiBold.ttf"),
-		"Raleway-Bold": require("../assets/fonts/Raleway-Bold.ttf"),
-		"Raleway-ExtraBold": require("../assets/fonts/Raleway-ExtraBold.ttf"),
-	});
+	const { loaded } = useFontCustom();
+
 	const [userNumber, setUserNumber] = useState(null);
 	const [name, setName] = useState("");
 	const [showWelcome, setShowWelcome] = useState(true);
