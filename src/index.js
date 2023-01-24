7499b53e7e2c965d161b5a83be439b5d11a828ae
Smael Nicolas <nico.smael@gmail.com>
2023-01-24 13:48:19 -0300

    added logic to navigate between screens

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 import { Header } from "./components";
-import { StartGame } from "./screens";
+import { colors } from "./constants/themes/colors";
+import { GameScreen, StartGame } from "./screens";
 import { styles } from "./styles";
 
 export const App = () => {
+	const [loaded, error] = useFonts({
+		"Karma-Regular": require("../assets/fonts/Karma-Regular.ttf"),
+		"Karma-Light": require("../assets/fonts/Karma-Light.ttf"),
+		"Karma-Medium": require("../assets/fonts/Karma-Medium.ttf"),
+		"Karma-SemiBold": require("../assets/fonts/Karma-SemiBold.ttf"),
+		"Karma-Bold": require("../assets/fonts/Karma-Bold.ttf"),
+	});
+	const [userNumber, setUserNumber] = useState(null);
+
+	const onStartGame = (selectedNumber) => {
+		setUserNumber(selectedNumber);
+	};
+
+	const Content = () =>
+		userNumber ? (
+			<GameScreen userNumber={userNumber} />
+		) : (
+			<StartGame onStartGame={onStartGame} />
+		);
+
+	if (!loaded) {
+		return (
+			<View style={styles.container}>
+				<ActivityIndicator size="large" color={colors.primary} />
+			</View>
+		);
+	}
+
 	return (
 		<View style={styles.container}>
 			<StatusBar style="auto" />
 			<Header title={"Adivine el numero"} />
-			<StartGame />
+			<Content />
 		</View>
 	);
 };
