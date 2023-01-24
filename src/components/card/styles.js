5e4193362aab4243f9b3259060b98d541ed4ce22
Smael Nicolas <nico.smael@gmail.com>
2023-01-24 15:42:16 -0300

    changed shadow

--- src/components/card/styles.js
+++ src/components/card/styles.js
@@ -3,17 +3,19 @@ import { colors } from "../../constants/themes/colors";
 
 export const styles = StyleSheet.create({
 	container: {
-		width: "75%",
-		padding: 20,
-		backgroundColor: colors.white,
+		width: "80%",
+		borderRadius: 3,
+		padding: 40,
+		backgroundColor: colors.text,
 		alignItems: "center",
-		shadowColor: colors.black,
+		shadowColor: colors.text,
 		shadowOffset: {
-			width: 0,
-			height: 1,
+			width: 1,
+			height: 2,
 		},
 		shadowOpacity: 0.22,
 		shadowRadius: 2.22,
 		elevation: 3,
+		marginVertical: 40,
 	},
 });
