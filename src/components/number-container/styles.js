6eacb1f7803d27a263a743c69c5508bc1faa4713
Smael Nicolas <nico.smael@gmail.com>
2023-01-24 15:41:56 -0300

    fix position

--- src/components/number-container/styles.js
+++ src/components/number-container/styles.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../constants/themes/colors";
 export const styles = StyleSheet.create({
 	number: {
-		fontSize: 14,
+		fontSize: 30,
+		padding: 5,
+		marginVertical: 10,
+		fontFamily: "Raleway-Bold",
+		color: colors.secondary,
 	},
 });
