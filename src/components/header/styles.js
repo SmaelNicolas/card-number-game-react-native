a9f7d4693b2ce884719f534b6fb1d0a313584b8a
Smael Nicolas <nico.smael@gmail.com>
2023-01-25 20:58:13 -0300

    fix header height

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -6,13 +6,13 @@ const { width, height } = Dimensions.get("screen");
 export const styles = StyleSheet.create({
 	container: {
 		backgroundColor: colors.secondary,
-		height: height * 0.15,
+		height: height * 0.2,
 		justifyContent: "center",
 		alignItems: "center",
 		paddingTop: 20,
 	},
 	title: {
-		fontSize: 25,
+		fontSize: 23,
 		color: colors.text,
 		fontFamily: "Raleway-SemiBold",
 		textTransform: "uppercase",
