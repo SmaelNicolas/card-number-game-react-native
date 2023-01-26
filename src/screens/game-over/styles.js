b955f84d7d4cdcbb8907bd123a5781653c8d645d
Smael Nicolas <nico.smael@gmail.com>
2023-01-25 22:03:25 -0300

    added styles to game over screen

---
+++ src/screens/game-over/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	content: {
+		width: "80%",
+		paddingVertical: 20,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	image: {
+		width: 200,
+		height: 200,
+	},
+	text: {
+		fontSize: 14,
+	},
+});
